3b9321c688260693d4b9b62e752f69d622b66d0c
mridul2507 <mridulsrivastava777@gmail.com>
2026-07-04 01:16:21 +0530

    WIP

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,9 +1,13 @@
 import { registerOTel } from "@vercel/otel";
 
+console.log("instrumentation.ts LOADED");
+
 export function register() {
-  console.log("OTEL REGISTERED");
+  console.log("register() CALLED");
 
   registerOTel({
     serviceName: "ir-assist-backend",
   });
+
+  console.log("registerOTel FINISHED");
 }
